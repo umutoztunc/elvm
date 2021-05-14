89f26c0b6ff8582da6e9e9887e3342e6bf5f662d
Umut Barış Öztunç <umutoztunc@gmail.com>
2021-05-14 10:59:25 +0000

    Little fix for ops array

--- ir/ir.c
+++ ir/ir.c
@@ -608,7 +608,8 @@ void dump_op(Op op, FILE* fp) {
   static const char* op_strs[] = {
     "mov", "add", "sub", "load", "store", "putc", "getc", "exit",
     "jeq", "jne", "jlt", "jgt", "jle", "jge", "jmp", "xxx",
-    "eq", "ne", "lt", "gt", "le", "ge", "dump"
+    "eq", "ne", "lt", "gt", "le", "ge", "dump",
+    "mul", "div", "mod",
   };
   fprintf(fp, "%s", op_strs[op]);
 }
